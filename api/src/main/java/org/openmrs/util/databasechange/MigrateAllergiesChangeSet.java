0258876c58b8c0a726fc64133da3e95de17bf50e
Siddharth Sharma <sid.sharma0@gmail.com>
2017-11-19 13:06:57 -0500

    fix sql query

--- api/src/main/java/org/openmrs/util/databasechange/MigrateAllergiesChangeSet.java
+++ api/src/main/java/org/openmrs/util/databasechange/MigrateAllergiesChangeSet.java
@@ -156,12 +156,16 @@ public class MigrateAllergiesChangeSet implements CustomTaskChange {
 	
 	private Integer getConceptByGlobalProperty(Database database, String globalPropertyName) throws Exception {
 		JdbcConnection connection = (JdbcConnection) database.getConnection();
-		Statement stmt = connection.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT property_value FROM global_property WHERE property = '" + globalPropertyName + "'");
+		String query = "SELECT property_value FROM global_property WHERE property = ?"
+		PreparedStatement stmt = connection.prepareStatement(query);
+		stmt.setString(1, globalPropertyName);
+		ResultSet rs = stmt.executeQuery();
 		if (rs.next()) {
 			String uuid = rs.getString("property_value");
-			
-			rs = stmt.executeQuery("SELECT concept_id FROM concept WHERE uuid = '" + uuid + "'");
+			query = "SELECT concept_id FROM concept WHERE uuid = ?";
+			stmt = connection.prepareStatement(query);
+			stmt.setString(1, uuid);
+			rs = stmt.executeQuery();
 			if (rs.next()) {
 				return rs.getInt("concept_id");
 			}
